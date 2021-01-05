991877ddc8b60c64e4ae07517d75af0be9f7a910
R.I.Pienaar <rip@devco.net>
2021-01-05 15:16:37 +0100

    (#83) restore ping as a low level test requirement
    Ping is a low level network tester that should remain available
    to the Ruby code as a way to confirm it functions.
    
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- lib/mcollective/application/ping.rb
+++ lib/mcollective/application/ping.rb
@@ -1,8 +1,36 @@
 module MCollective
   class Application::Ping < Application # rubocop:disable Style/ClassAndModuleChildren
-    description "Ping all nodes"
+    description "Low level network connectivity test"
 
-    external(:command => "choria", :args => ["ping"])
-    external_help(:command => "choria", :args => ["ping", "--help"])
+    def main
+      # If the user did not override the default timeout include the discovery timeout
+      if options[:timeout] == 5
+        discovery_timeout = options[:disctimeout] || Config.instance.discovery_timeout || 0
+        options[:timeout] = options[:timeout] + discovery_timeout
+      end
+      client = MCollective::Client.new(options)
+
+      start = Time.now.to_f
+      times = []
+
+      client.req("ping", "discovery") do |resp|
+        times << (Time.now.to_f - start) * 1000
+
+        puts "%-40s time=%.2f ms" % [resp[:senderid], times.last]
+      end
+
+      puts("\n\n---- ping statistics ----")
+
+      if !times.empty?
+        sum = times.inject(0) {|acc, i| acc + i}
+        avg = sum / times.length.to_f
+
+        puts "%d replies max: %.2f min: %.2f avg: %.2f" % [times.size, times.max, times.min, avg]
+      else
+        puts("No responses received")
+      end
+
+      halt client.stats
+    end
   end
 end
